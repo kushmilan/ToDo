29723ec636afb6861b40207933f0a4056eb4dfe2
Milan Singh Kushwah <33677132+kushmilan@users.noreply.github.com>
2021-05-31 09:11:50 +0530

    Updated isValid msg

--- src/main/java/firstwebapp/login/LoginCheck.java
+++ src/main/java/firstwebapp/login/LoginCheck.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 public class LoginCheck {
 	
 	public boolean isValid(String name, String password){
-		System.out.println("Working here");
 		return name.equals("milan") && password.equals("abcd");
 	}
 }
